8b665c27cbe56820dc170b78d95b7174163b7a14
Cory Bretsch <corybretsch@gmail.com>
2024-06-03 10:33:12 -0500

    test: class Biker #initialize

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -1,2 +1,22 @@
 require './lib/ride'
 require './lib/biker'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Biker do
+  describe '#initialize' do
+    it 'exists with attributes' do
+      biker = Biker.new("Kenny", 30)
+      
+      expect(biker).to be_a Biker
+      expect(biker.name).to eq ("Kenny")
+      expect(biker.max_distance).to eq 30
+      expect(biker.rides).to eq ({})
+      expect(biker.acceptable_terrain).to eq []
+    end
+  end
+
+
+end
