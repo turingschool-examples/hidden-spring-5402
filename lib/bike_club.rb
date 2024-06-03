e756b92268a55e9e14af842b7fca2171258b6eea
Tyler Noble <tmnoble11@gmail.com>
2024-06-03 11:54:29 -0600

    Added: most_rides and test

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -2,12 +2,21 @@
 
 class BikeClub
     attr_accessor :bikers
-    attr_reader :name
+    attr_reader :name, :bikers
 
     def initialize(name)
         @name = name
         @bikers = []
     end
 
+    def add_biker(biker)
+        @bikers << biker
+    end
     
+    #checks bikers rides by accessing @bikers, and returns the biker with the most rides. max_by is a method that returns the element that has the maximum value of the block.
+    def most_rides
+        @bikers.max_by do |biker|
+            biker.rides.length
+        end
+    end
 end
--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -15,4 +15,46 @@ RSpec.describe BikeClub do
         bike_club = BikeClub.new("Sons of Anarchy")
         expect(bike_club.bikers).to eq([])
     end
+
+    describe 'add_biker' do
+        it 'can add bikers' do
+            bike_club = BikeClub.new("Sons of Anarchy")
+            biker = Biker.new("Sal", 30)
+            bike_club.add_biker(biker)
+            expect(bike_club.bikers).to eq([biker])
+        end
+    end
+
+    describe 'most_rides' do
+        it 'can find the biker with the most rides' do
+            bike_club = BikeClub.new("Sons of Anarchy")
+            biker1 = Biker.new("Sal", 30)
+            biker2 = Biker.new("Sam", 13)
+            biker3 = Biker.new("Sue", 20)
+            biker1.learn_terrain!(:gravel)
+            biker2.learn_terrain!(:gravel)
+            biker3.learn_terrain!(:gravel)
+
+            bike_club.add_biker(biker1)
+            bike_club.add_biker(biker2)
+            bike_club.add_biker(biker3)
+
+            ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10, loop: true, terrain: :gravel})
+            ride2 = Ride.new({name: "Cherry Creek Trail", distance: 7, loop: false, terrain: :gravel})
+            ride3 = Ride.new({name: "Mountain Pass", distance: 14, loop: false, terrain: :gravel})
+
+            biker1.log_ride(ride1, 60.2)
+            biker1.log_ride(ride2, 40.2)
+            biker1.log_ride(ride3, 30.2)
+            biker2.log_ride(ride1, 60.2)
+            biker2.log_ride(ride2, 40.2)
+            biker3.log_ride(ride1, 60.2)
+            biker3.log_ride(ride2, 40.2)
+
+            expect(bike_club.most_rides).to eq(biker1)
+            expect(biker1.rides.length).to eq(3)
+            expect(biker2.rides.length).to eq(1)
+            expect(biker3.rides.length).to eq(2)
+        end
+    end
 end
