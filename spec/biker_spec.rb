945d6b0e0abf76c91195a7691d5df50b0b3519a4
Karl Fallenius <smilodon.p@gmail.com>
2024-06-03 11:50:25 -0600

    ran out of time trying to finish iteration 2

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Biker do
       expect(@biker1.rides).to eq({})
       expect(@biker1.acceptable_terrain).to eq([])
     end
-    it "can initialize a differetmt biker" do
+    it "can initialize a different biker" do
       expect(@biker2.name).to eq("Athena")
       expect(@biker2.max_distance).to eq(15)
       expect(@biker2.rides).to eq({})
@@ -30,16 +30,37 @@ RSpec.describe Biker do
     expect(@biker1.acceptable_terrain).to eq([:gravel, :hills])
   end
 
-  it "can log a ride" do 
+  it "can log rides" do 
     @biker1.log_ride(@ride1, 92.5)
+    expect(@biker1.rides).to eq({@ride1 => [92.5]})
+
     @biker1.log_ride(@ride1, 91.1)
+    expect(@biker1.rides).to eq({@ride1 => [92.5, 91.1]})
+
     @biker1.log_ride(@ride2, 60.9)
     @biker1.log_ride(@ride2, 61.6)
-
-    epxect(@biker1.rides).to eq({@ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6])
+    expect(@biker1.rides).to eq({@ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6]})
   end
 
-  it "can return a rider's personal best times from logged rides" do
+  describe "can protect a rider from themselves" do
+    it "can keep riders off of terrain types they can't ride yet" do
+      expect(@biker2.log_ride(@ride2, 67.0)).to eq("you don't know how to ride this type of terrain yet")
+    end
+    it "can keep riders off of routes that are too long" do
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+      expect(@biker2.log_ride(@ride1, 97.0)).to eq("you are currently unable to bike this distance")
+    end
+    it "notifies biker when they have chosen a route for which they possess the requisite ability" do
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+      expect(@biker2.log_ride(ride2, 65.0)).to eq("You know this terrain and can bike this distance")
+    end
+  end
+
+  xit "can return a rider's personal best times from logged rides" do
     
   end
+  # end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require './lib/ride.rb'
 require './lib/biker.rb'
-require './lib/bike_club.rb'
+# require './lib/bike_club.rb'
 
 RSpec.configure do |config|
   config.formatter = :documentation
