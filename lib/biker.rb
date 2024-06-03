3da4ec97543ddcfd3fecb9b205feee20623fc86f
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-06-03 12:23:33 -0600

    created biker class

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,15 @@
 class Biker
+    attr_reader :name, :acceptable_terrain, :max_distance
 
+    def name
+    end 
+    
+    def acceptable_terrain
+    end
+
+    def max_distance
+    end
 end
+
+biker1 = Biker1.new({name: "Kenny", acceptable_terrain: ["gravel", "hills"] , max_distance: 30})
+biker2 = Biker2.new({name: "Athena", acceptable_terrain: ["gravel", "hills"], max_distance: 65})
