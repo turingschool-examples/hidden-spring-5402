da854df22bd877166a6d7b2d17cfc37ca4a15629
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-06-03 09:43:50 -0600

    Create tests for ride class

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -1,39 +1,63 @@
+require './spec/spec_helper'
+RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    # pry(main)> ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    # # => #<Ride:0x00007f845c279ee8...>
+    
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    # pry(main)> ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    # # => #<Ride:0x00007f845c228b38...>
+  end
+
+  it "can initialize" do
+    expect(@ride1).to be_an_instance_of(Ride)
+    expect(@ride2).to be_an_instance_of(Ride)
+  end
+
+  it "has a name" do
+    expect(@ride1.name).to eq("Walnut Creek Trail")
+    # pry(main)> ride1.name
+    # # => "Walnut Creek Trail"
+  end
+
+  it "has a distance" do
+    expect(@ride1.distance).to eq(10.7)
+    # pry(main)> ride1.distance
+    # # => 10.7
+  end
+
+  it "is either a loop or not" do
+    expect(@ride1.loop?).to eq(false)
+    # pry(main)> ride1.loop?
+    # # => false
+
+    expect(@ride2.loop?).to eq(true)
+    # pry(main)> ride2.loop?
+    # # => true
+  end
+
+  it "has a terrain type" do
+    expect(@ride1.terrain).to eq(:hills)
+    # pry(main)> ride1.terrain
+    # # => :hills
+  end
+    
+  it "doubles the distance for total_distance if not a loop" do
+    expect(@ride1.total_distance).to eq(14.9)
+    # pry(main)> ride2.total_distance
+    # # => 14.9
+  
+    expect(@ride2.total_distance).to eq(21.4)
+    # pry(main)> ride1.total_distance
+    # # => 21.4
+  end
+end
 
 
 
 
-# Iteration 1 - Rides
-# Graded Items:
 
-# Create Rides with attributes and a way to read that data
-# Rides can indicate if the ride is a loop or not.
-# A Ride can report its total distance. The total distance is double the base distance if the ride is not a loop.
-# pry(main)> require './lib/ride'
-# # => true
 
-# pry(main)> ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-# # => #<Ride:0x00007f845c279ee8...>
 
-# pry(main)> ride1.name
-# # => "Walnut Creek Trail"
 
-# pry(main)> ride1.distance
-# # => 10.7
-
-# pry(main)> ride1.terrain
-# # => :hills
-
-# pry(main)> ride1.loop?
-# # => false
-
-# pry(main)> ride1.total_distance
-# # => 21.4
-
-# pry(main)> ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
-# # => #<Ride:0x00007f845c228b38...>
-
-# pry(main)> ride2.loop?
-# # => true
-
-# pry(main)> ride2.total_distance
-# # => 14.9
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require './lib/ride'
+require './lib/biker'
+require './lib/bike_club'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
