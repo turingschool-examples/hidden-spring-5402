f22bef956a9ae68992c62db081e5b4a1a57c3cec
Tyler Noble <tmnoble11@gmail.com>
2024-06-03 11:30:04 -0600

    Added: initial tests bike_club

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,13 @@
-class BikeClub
 
+
+class BikeClub
+    attr_accessor :bikers
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bikers = []
+    end
+
+    
 end
--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -1,1 +1,18 @@
 require 'spec_helper'
+
+RSpec.describe BikeClub do
+    it 'exists' do
+        bike_club = BikeClub.new("Sons of Anarchy")
+        expect(bike_club).to be_a(BikeClub)
+    end
+
+    it 'has a name' do
+        bike_club = BikeClub.new("Sons of Anarchy")
+        expect(bike_club.name).to eq("Sons of Anarchy")
+    end
+
+    it 'has bikers' do
+        bike_club = BikeClub.new("Sons of Anarchy")
+        expect(bike_club.bikers).to eq([])
+    end
+end
