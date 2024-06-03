d468e8a4c9e02b8af1d95da02847c23b600dd846
Cory Bretsch <corybretsch@gmail.com>
2024-06-03 10:48:22 -0500

    test: class Biker add #log_ride

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -28,5 +28,20 @@ RSpec.describe Biker do
     end
   end
 
+  describe '#log_ride' do
+    it 'can log a ride in attribute @rides hash' do
+      biker = Biker.new("Kenny", 30)
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.log_ride(ride1, 92.5)      
+      biker.log_ride(ride1, 91.1)      
+      biker.log_ride(ride2, 60.9)      
+      biker.log_ride(ride2, 61.6)
+
+      expect(biker.rides).to eq({ride1 => [92.5, 91.1], ride2 => [60.9, 61.6]})
+    end
+  end
 
 end
