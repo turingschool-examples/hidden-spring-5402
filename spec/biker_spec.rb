208a8f1aace930020249ad3ba7fc35cbe5970b88
Cory Bretsch <corybretsch@gmail.com>
2024-06-03 11:41:42 -0500

    test: Biker #personal_record

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -44,4 +44,20 @@ RSpec.describe Biker do
     end
   end
 
+  describe '#personal_record' do
+    it 'can return lowest time when passed a ride route'
+      biker = Biker.new("Kenny", 30)
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.log_ride(ride1, 92.5)      
+      biker.log_ride(ride1, 91.1)      
+      biker.log_ride(ride2, 60.9)      
+      biker.log_ride(ride2, 61.6)
+
+      expect(biker.personal_record(ride1)).to eq 91.1
+      expect(biker.personal_record(ride2)).to eq 60.9
+  end
+
 end
