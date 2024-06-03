728c55cf9c104f4bb57bc4eda3bddc860ab72cf3
Cory Bretsch <corybretsch@gmail.com>
2024-06-03 11:36:04 -0500

    feat: added very difficult method (lordygit add .) log_ride

--- lib/biker.rb
+++ lib/biker.rb
@@ -7,11 +7,20 @@ class Biker
   def initialize(name, max_distance)
     @name = name
     @max_distance = max_distance
-    @rides = {}
+    @rides = Hash.new()
     @acceptable_terrain = []
   end
 
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
+  end
+
+  #this method took me an hour to solve. lordy that was hard!!
+  def log_ride(ride_object, time)
+    if @rides.keys.include?(ride_object)
+      @rides[ride_object] << time
+    else
+      @rides[ride_object] = [time]
+    end
   end
 end
