c3bc8dcce9a4d441b8e2d510e322b67c47054adc
Cory Bretsch <corybretsch@gmail.com>
2024-06-03 12:59:47 -0500

    last commit

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,4 +1,23 @@
 class BikeClub
+  attr_reader :name, :bikers
   
+  def initialize(name)
+    @name = name
+    @bikers = []
+  end
 
+  def add_bikers(biker)
+    @bikers << biker
+  end
+
+  def most_rides
+    hash = Hash.new(0)
+    @bikers.each do |biker|
+      hash[biker] = biker.rides.values.flatten.count
+    end
+    value = hash.max_by do |biker_object, ride_count|
+      ride_count
+    end
+    value[0].name
+  end
 end
