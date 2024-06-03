b65749be591249ca2bc05ef57cc6b910522d22ed
Cory Bretsch <corybretsch@gmail.com>
2024-06-03 10:36:48 -0500

    feat: class Biker #initialize

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,13 @@
 class Biker
+  attr_reader :name, 
+              :max_distance, 
+              :rides, 
+              :acceptable_terrain
 
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+    @rides = {}
+    @acceptable_terrain = []
+  end
 end
